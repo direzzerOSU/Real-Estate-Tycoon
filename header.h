647f6a7d71e393f4f49f6229a12768886218d4ab
Ryan DiRezze <direzzer@oregonstate.edu>
2018-10-23 16:50:04 -0700

    previous updates

---
+++ header.h
@@ -0,0 +1,14 @@
+// header with classes
+
+#ifndef TYCOON_H
+#define TYCOON_H
+
+#include<iostream>
+#include<string>
+// #include<fstream>
+// #include<map>
+
+// player starts with $500k; loses with $0; wins with $1m
+
+
+#endif
---
+++ implementation.cpp
@@ -0,0 +1,17 @@
+#include "header.h"
+// #include<cctype>
+// #include<locale>
+// #include<sstream>
+
+using std::ifstream;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+
+int main(){
+
+  return 0;
+}
